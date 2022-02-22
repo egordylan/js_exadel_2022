5d0a936398038a8c28b2a63cdf6414941ca60f3d
egordylan <egordylan@gmail.com>
2022-02-22 12:28:04 +0300

    refactor: task3

--- 1. Week 1/task3.js
+++ 1. Week 1/task3.js
@@ -11,15 +11,14 @@ const prices = [64, 7556, 345, 7556, 345, 7556, 345, 7556, 433, 345, 756, 123,
                 942, 3112, 421, 9341, 1212, 8, 43, 41, 345, 341, 21, 321, 123];
 
 
-function sum(array){
+function calcSummaAndAverage(array){
   let summa = 0;
-  const length = array.length;
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < array.length; i++) {
     summa += array[i];
   }
-  let average = summa / length;
+  const average = summa / array.length;
 
   return `Total: ${summa} hryvnias, average price of goods ${average} hryvnias.`;
 }
 
-console.log(sum(prices));
+console.log(calcSummaAndAverage(prices));
